5bab096ca8f6637d8025f6086d9723b3e037dd4a
Alpha-King-MD <alphakingmd1@gmail.com>
2026-02-18 06:25:09 +0000

    update bot commands

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     reqno: '94766357263', // රික්වෙස්ට් මැසේජ් යන්න ඕන නම්බර් එක (94 එක්ක)
     prefix: ".", 
     logoPath: "./Media/Images/logo.png", // මෙන්න මෙතනට ලෝගෝ එකේ path එක දුන්නා
-    groups: ['120363407305281183@g.us', '120363406667808399@g.us', '22330189328469@lid'],
+    groups: ['120363407305281183@g.us', '120363406667808399@g.us', '22330189328469@lid', '120363423691940922@g.us'],
     workMode: "groups", // 'all' (සෑම තැනකම), 'groups' (ගෲප් වල පමණි), 'private' (Direct messages පමණි)
     geminiKey: 'AIzaSyBQ1gZ01DEQ5b5FjZH1si_-3j0G6N4MrRU',
     removeBgApiKey: "YzBWMFht4vz9LAw9xF2zmKau" 
