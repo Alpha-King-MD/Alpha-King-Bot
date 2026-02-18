f937f674adf9af4ae02b6711506e38a110c1c05e
Alpha-King-MD <alphakingmd1@gmail.com>
2026-02-18 09:13:54 +0000

    Update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     botName: "Alpha King",
     ownerName: "Anu",
-    owner: '94777892536', // ඔයාගේ ප්‍රධාන නම්බර් එක
+    owner: ['94777892536', '94766357263'], // ඔයාගේ ප්‍රධාන නම්බර් එක
     reqno: '94766357263', // රික්වෙස්ට් මැසේජ් යන්න ඕන නම්බර් එක (94 එක්ක)
     prefix: ".", 
     logoPath: "./Media/Images/logo.png", // මෙන්න මෙතනට ලෝගෝ එකේ path එක දුන්නා
